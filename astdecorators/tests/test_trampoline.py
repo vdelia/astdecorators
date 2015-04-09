7f9f2c962282c19cb0ca49aa3836a1001c4a91d3
Vincenzo D\'Elia <vincenzo.delia@amadeus.com>
2015-04-09 18:57:44 +0200

    test fibonacci sequence

--- astdecorators/tests/test_trampoline.py
+++ astdecorators/tests/test_trampoline.py
@@ -6,6 +6,11 @@ def factorial(x, acc=1):
         return acc
     return factorial(x-1, acc=acc*x)
 
+def fib(n):
+    def fib_help(a, b, n):
+        return fib_help(b, a+b, n-1) if n > 0 else a
+    return fib_help(0, 1, n)
+
 class TestTailRecElimination(unittest.TestCase):
 
     def test_factorial(self):
@@ -14,8 +19,12 @@ class TestTailRecElimination(unittest.TestCase):
         tfactorial = trampolined(factorial)
         self.assertEqual(3628800,tfactorial(10))
 
-        try:
-            tfactorial(1000)
-        except:
-            self.assertFalse(True, "no max recursion depth exceeded")
+        tfactorial(1000)
+
+    def test_fib(self):
+        tfib = trampolined(fib)
+        self.assertRaises(RuntimeError, lambda: fib(1000))
+        self.assertEqual(55, tfib(10))
+
+        tfib(10000)
 
