44f333b20df46594101981d8f605c690ca599c0d
Vincenzo D\'Elia <vincenzo.delia@amadeus.com>
2015-04-09 18:06:50 +0200

    trampoline decorator

--- astdecorators/tests/test_trampoline.py
+++ astdecorators/tests/test_trampoline.py
@@ -2,7 +2,6 @@ import unittest
 from astdecorators.trampoline import trampolined
 
 def factorial(x, acc=1):
-    assert x >= 0
     if x == 0:
         return acc
     return factorial(x-1, acc=acc*x)
@@ -12,5 +11,11 @@ class TestTailRecElimination(unittest.TestCase):
     def test_factorial(self):
         self.assertRaises(RuntimeError, lambda: factorial(1000))
 
-        decorated = trampolined(factorial)
+        tfactorial = trampolined(factorial)
+        self.assertEqual(3628800,tfactorial(10))
+
+        try:
+            tfactorial(1000)
+        except:
+            self.assertFalse(True, "no max recursion depth exceeded")
 
--- astdecorators/trampoline.py
+++ astdecorators/trampoline.py
@@ -1,5 +1,5 @@
 import decorator
-
+import ast, meta
 
 def trampoline(f, *args, **kwargs):
     g = f(*args, **kwargs)
@@ -7,7 +7,29 @@ def trampoline(f, *args, **kwargs):
         g = g()
     return g
 
+class RewriteReturn(ast.NodeTransformer):
+
+    def visit_Return(self, node):
+        newnode = ast.Return(
+                value=ast.Lambda(
+                    args=ast.arguments(
+                        args=[],
+                        vararg=None,
+                        kwarg=None, defaults=[]),
+                    body=node.value),
+                    )
+        return ast.copy_location(newnode, node)
+
 @decorator.decorator
 def trampolined(f, *args, **kwargs):
-    pass
+
+    tree = meta.decompiler.decompile_func(f)
+    transformed_tree = RewriteReturn().visit(tree)
+    ast.fix_missing_locations(transformed_tree)
+
+    scope = dict(globals())
+    f = meta.decompiler.compile_func(transformed_tree, '', scope)
+    scope[f.func_name] = f
+    
+    return trampoline(f, *args, **kwargs)
 
